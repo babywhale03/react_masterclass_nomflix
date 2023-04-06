e13ca7370b7404dea3f5ac5a3fed7e991425fc95
babywhale03 <inthemode@naver.com>
2023-04-07 00:16:15 +0900

    [04.07] #9.5

---
+++ src/Routes/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "79f6ae725e2b86d19121dfa00f3b267c";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+    return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+        (response) => response.json()
+    );
+}
